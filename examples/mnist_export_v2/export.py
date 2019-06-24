be6df74e631609989258b6c72c62811344177ab6
captain-pool <rickdey1998@gmail.com>
2019-06-24 13:50:31 +0530

    Added License

--- examples/mnist_export_v2/export.py
+++ examples/mnist_export_v2/export.py
@@ -1,14 +1,32 @@
+# Copyright 2018 The TensorFlow Hub Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+r"""Exporter for TF-Hub Modules in SavedModel v2.0 format.
+
+The module has as a single signature, accepting a batch of images with shape [None, 28, 28, 1] and returning a prediction vector.
+In this example, we are loading the MNIST Dataset from TFDS and training a simple digit classifier.
+"""
+
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import division
+
 import sys
 import tensorflow as tf
 import tensorflow_datasets as tfds
 import tensorflow_hub as hub
 import argparse
-
-"""Exporter for TF-Hub Modules in SavedModel v2.0 format.
-
-The module has as a single signature, accepting a batch of images with shape [None, 28, 28, 1] and returning a prediction vector.
-In this example, we are loading the MNIST Dataset from TFDS and training a simple digit classifier.
-"""
 
 FLAGS = None
 
--- examples/mnist_export_v2/export_test.py
+++ examples/mnist_export_v2/export_test.py
@@ -1,9 +1,26 @@
+# Copyright 2018 The TensorFlow Hub Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+r""" Tests for MNIST exporter."""
+
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import division
+
 import tensorflow as tf
 import tensorflow_hub as hub
-import unittest
-import tempfile
 import os
-import re
 import export
 
 
