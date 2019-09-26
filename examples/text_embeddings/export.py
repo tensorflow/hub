c689906edb06a93757adccd099569d9599f02c80
TensorFlow Hub Authors <no-reply@google.com>
2019-09-26 13:29:05 +0200

    Replace missing use of tf.contrib.lookup.index_table_from_file
    PiperOrigin-RevId: 271322574

--- examples/text_embeddings/export.py
+++ examples/text_embeddings/export.py
@@ -142,10 +142,12 @@ def make_module_spec(vocabulary_file, vocab_size, embeddings_dim,
         initializer=tf.zeros([vocab_size + num_oov_buckets, embeddings_dim]),
         name=EMBEDDINGS_VAR_NAME,
         dtype=tf.float32)
-    lookup_table = tf.contrib.lookup.index_table_from_file(
-        vocabulary_file=vocabulary_file,
-        num_oov_buckets=num_oov_buckets,
-    )
+    table_initializer = tf.lookup.TextFileInitializer(
+        vocabulary_file,
+        tf.string, tf.lookup.TextFileIndex.WHOLE_LINE,
+        tf.int64, tf.lookup.TextFileIndex.LINE_NUMBER)
+    lookup_table = tf.lookup.StaticVocabularyTable(
+        table_initializer, num_oov_buckets=num_oov_buckets)
     sparse_ids = tf.SparseTensor(
         indices=tokens.indices,
         values=lookup_table.lookup(tokens.values),
