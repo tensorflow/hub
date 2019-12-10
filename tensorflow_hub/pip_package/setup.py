f68cefdc2e31f032628d04d8a481b2ff8919ed9a
TensorFlow Hub Authors <no-reply@google.com>
2019-12-10 19:31:48 +0100

    PIP package tensorflow-hub gets upgraded requirements, in line with current tensorflow, and actual usage.
    In particular, protobuf should not be less than what WORKSPACE uses.
    
    PiperOrigin-RevId: 284798869

--- tensorflow_hub/pip_package/setup.py
+++ tensorflow_hub/pip_package/setup.py
@@ -30,8 +30,8 @@ with open("tensorflow_hub/version.py") as in_file:
 
 REQUIRED_PACKAGES = [
     'numpy >= 1.12.0',
-    'six >= 1.10.0',
-    'protobuf >= 3.4.0',
+    'six >= 1.12.0',
+    'protobuf >= 3.8.0',  # No less than what ../WORKSPACE uses.
 ]
 
 project_name = 'tensorflow-hub'
