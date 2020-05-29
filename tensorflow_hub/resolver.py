8393fa1ea6f76f8a8d56ef4b17e2dccd8adaa176
Jin Dong <jin.dong@mail.mcgill.ca>
2020-05-29 01:39:07 -0400

    also check model files

--- tensorflow_hub/resolver.py
+++ tensorflow_hub/resolver.py
@@ -380,7 +380,10 @@ def atomic_download(handle,
                                              overwrite=False)
         # Must test condition again, since another process could have created
         # the module and deleted the old lock file since last test.
-        if tf_v1.gfile.Exists(module_dir):
+        if tf_v1.gfile.Exists(module_dir) and (
+          tf_v1.gfile.Exists(os.path.join(module_dir, "saved_model.pbtxt")) or
+          tf_v1.gfile.Exists(os.path.join(module_dir, "saved_model.pb"))
+        ):
           # Lock file will be deleted in the finally-clause.
           return module_dir
         break  # Proceed to downloading the module.
