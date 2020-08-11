7596f2b5f5bf89287fd66ae73affe4a2896be147
TensorFlow Hub Authors <no-reply@google.com>
2020-08-11 08:00:22 -0700

    Update docstring: share_cluster_devices_in_session is not experimental anymore.
    PiperOrigin-RevId: 326021162

--- tensorflow_hub/keras_layer.py
+++ tensorflow_hub/keras_layer.py
@@ -84,8 +84,8 @@ class KerasLayer(tf.keras.layers.Layer):
   explicitly from Keras objects instead of relying on graph collections.
   This layer class does not support graph collections.
   Distributed training of the Estimator requires setting the option
-  `session_config.experimental.share_cluster_devices_in_session` within
-  the `tf.estimator.RunConfig`. (It becomes non-experimental in TF2.2.)
+  `session_config.share_cluster_devices_in_session` within the
+  `tf.estimator.RunConfig`. (This option was experimental from TF1.14 to TF2.1.)
 
   Note: The data types used by a saved model have been fixed at saving time.
   Using tf.keras.mixed_precision etc. has no effect on the saved model
