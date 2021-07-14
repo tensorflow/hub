Fix issue when a mixed precision model with a KerasLayer is saved.
The issue occurred when a mixed precision model uses a KerasLayer wrapping a float32 SavedModel. If the mixed precision model is then saved with an optimizer with slot variables, an error would occur. The issue was that KerasLayer would wrap the SavedModel's variables with AutoCastVariables, which would put both the variables and the AutoCastVariables wrapping them in the checkpoint. This causes issues when there are slot variables.

To fix, KerasLayer no longer creates AutoCastVariables.

PiperOrigin-RevId: 384744914

tensorflow_hub/keras_layer.py
@@ -201,7 +201,8 @@ class KerasLayer(tf.keras.layers.Layer):
     """Calls add_weight() to register but not create an existing weight."""
     if trainable is None: trainable = weight.trainable
     self.add_weight(name=weight.name, shape=weight.shape, dtype=weight.dtype,
-                    trainable=trainable, getter=lambda *_, **__: weight)
+                    trainable=trainable, experimental_autocast=False,
+                    getter=lambda *_, **__: weight)
 
   def _call_loss_if_trainable(self, loss):
     """Returns `loss` conditioned on whether this layer is trainable."""