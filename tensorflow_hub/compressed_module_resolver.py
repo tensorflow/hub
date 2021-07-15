41accd5119cd6d7958cf6cae1339a667807a1b0f
Vedant Mehta <65880150+vmehta-11@users.noreply.github.com>
2021-07-15 15:02:56 -0400

    Update compressed_module_resolver.py

--- tensorflow_hub/compressed_module_resolver.py
+++ tensorflow_hub/compressed_module_resolver.py
@@ -18,11 +18,11 @@ import hashlib
 import urllib
 
 import tensorflow as tf
-from tensorflow_hub import resolver
+from tensorflow_hub import resolver, _bypass_cert_validation_check
 
 
 LOCK_FILE_TIMEOUT_SEC = 10 * 60  # 10 minutes
-
+TF_HUB_DISABLE_CERT_VALIDATION_CHECK = _bypass_cert_validation_check()
 _COMPRESSED_FORMAT_QUERY = ("tf-hub-format", "compressed")
 
 
@@ -60,7 +60,7 @@ class HttpCompressedFileResolver(resolver.HttpResolverBase):
       """Fetch a module via HTTP(S), handling redirect and download headers."""
       request = urllib.request.Request(
           self._append_compressed_format_query(handle))
-      response = self._call_urlopen(request)
+      response = self._call_urlopen(request, bypass_cert_validation=TF_HUB_DISABLE_CERT_VALIDATION_CHECK)
       return resolver.DownloadManager(handle).download_and_uncompress(
           response, tmp_dir)
 
