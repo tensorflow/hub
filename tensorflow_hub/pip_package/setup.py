141e560b9a02832678125ba88d9c246cafa4ad75
TensorFlow Hub Authors <no-reply@google.com>
2023-03-13 11:19:42 -0700

    Restrict protobuf to be >= 3.8.0, <4
    PiperOrigin-RevId: 516267032

--- tensorflow_hub/pip_package/setup.py
+++ tensorflow_hub/pip_package/setup.py
@@ -27,7 +27,7 @@ with open('tensorflow_hub/version.py') as in_file:
 
 REQUIRED_PACKAGES = [
     'numpy >= 1.12.0',
-    'protobuf >= 3.8.0',  # No less than what ../WORKSPACE uses.
+    'protobuf >= 3.8.0, < 4.0',  # No less than what ../WORKSPACE uses.
 ]
 
 project_name = 'tensorflow-hub'
