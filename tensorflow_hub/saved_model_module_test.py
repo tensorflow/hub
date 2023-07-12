c8b88a187a6ad512ccec84c8f163bb7df79c3b8f
TensorFlow Hub Authors <no-reply@google.com>
2023-07-12 06:24:32 -0700

    Create the SavedModel via TF 2
    PiperOrigin-RevId: 547477364

--- tensorflow_hub/saved_model_module_test.py
+++ tensorflow_hub/saved_model_module_test.py
@@ -20,41 +20,49 @@ import tensorflow as tf
 import tensorflow_hub as hub
 
 
-_EXTRA_COLLECTION = "exercise_drop_collection"
+def _double(input_):
+  return input_ * 2
+
+
+class MyModel(tf.Module):
+
+  @tf.function
+  def __call__(self, input_):
+    return _double(input_)
 
 
 class SavedModelTest(tf.test.TestCase):
 
-  def createSavedModel(self):
+  def _create_tf2_saved_model(self):
     model_dir = os.path.join(self.get_temp_dir(), "saved_model")
-    with tf.Graph().as_default():
-      x = tf.compat.v1.placeholder(dtype=tf.float32, shape=[None, 3])
-      w = tf.compat.v1.get_variable("weights", shape=[])
-      y = x*w
-      tf.compat.v1.add_to_collection(_EXTRA_COLLECTION, y)
+    model = MyModel()
 
-      init_op = tf.compat.v1.assign(w, 2)
+    @tf.function
+    def serving_default(input_):
+      return {"output": model(input_)}
 
-      with tf.compat.v1.Session() as session:
-        session.run(init_op)
-        tf.compat.v1.saved_model.simple_save(
-            session,
-            model_dir,
-            inputs={"x": x},
-            outputs={"y": y},
-        )
+    signature_function = serving_default.get_concrete_function(
+        tf.TensorSpec(shape=[3,], dtype=tf.float32)
+    )
+    tf.saved_model.save(
+        model,
+        model_dir,
+        signatures={
+            tf.saved_model.DEFAULT_SERVING_SIGNATURE_DEF_KEY: signature_function
+        },
+    )
+
     return model_dir
 
   def testLoadSavedModel(self):
-    saved_model_path = self.createSavedModel()
-    spec = hub.create_module_spec_from_saved_model(
-        saved_model_path,
-        drop_collections=[_EXTRA_COLLECTION])
-    with tf.Graph().as_default():
-      m = hub.Module(spec, tags=["serve"])
-      y = m([[2, 4, 5]], signature="serving_default", as_dict=True)["y"]
-      with tf.compat.v1.train.MonitoredSession() as session:
-        self.assertAllEqual(session.run(y), [[4, 8, 10]])
+    saved_model_path = self._create_tf2_saved_model()
+    loaded = hub.load(saved_model_path)
+    self.assertAllClose(
+        loaded.signatures[tf.saved_model.DEFAULT_SERVING_SIGNATURE_DEF_KEY](
+            tf.constant([2.0, 4.0, 5.0])
+        )["output"],
+        tf.constant([4.0, 8.0, 10.0]),
+    )
 
 
 if __name__ == "__main__":
